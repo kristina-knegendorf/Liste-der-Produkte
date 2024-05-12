c9e9dd46ed67e127324c34f298506c7f802942d1
kristina-knegendorf <159029314+kristina-knegendorf@users.noreply.github.com>
2024-05-12 20:55:53 +0200

    Add files via upload

---
+++ Liste der Produkte/src/data.js
@@ -0,0 +1,87 @@
+//array that carries all the data of the products that are listed on the shop page
+let shopItemsData = [
+    {
+        id:"shoe1",
+        name: "Air Force 1 Mid",
+        price: 130,
+        desc:"Der Air Force 1 Mid besticht mit den bewährten Details – geschmeidigen Überzügen, markanten Akzenten und einen glänzenden Look",
+        img: "img/air-force-1-mid.png"
+    },
+    {
+        id:"shoe2",
+        name: "Air Max 270",
+        price: 130,
+        desc:"Als erster alltagstauglicher Air Max von Nike bietet der Nike Air Max 270 Tragekomfort und viel Style.",
+        img: "img/air-max-270-.png"
+    },
+    {
+        id:"shoe3",
+        name: "Air Max Plus",
+        price: 110,
+        desc:"Der Nike Air Max Plus behält die legendäre Tuned Air-Dämpfung und die energiegeladenen Designlinien des Originals aus dem Jahr 1998.",
+        img: "img/air-max-plus-schuh.png"
+    },
+    {
+        id:"shoe4",
+        name: "Dunk High Retro",
+        price: 120,
+        desc:"Der Basketball-Klassiker der 80er wurde für das Hallenparkett entwickelt und ist mit perfekt schimmernden Überzügen und originalen University-Farben wieder zurück.",
+        img: "img/dunk-high-retro-.png"
+    }, 
+    {
+        id:"shoe5",
+        name: "Air Max Dn",
+        price: 170,
+        desc:" Der neue Nike Air Max Dn sorgt dank des Dynamic Air-Elements, das aus zwei Tubes mit unterschiedlichem Druck besteht, bei jedem Schritt für ein reaktionsfreudiges Tragegefühl",
+        img: "img/air-max-dn-.png"
+    },
+    {
+        id:"shoe6",
+        name: "Air Force 1 07 Next Nature SE",
+        price: 120,
+        desc:"Die klassische 80er-Jahre-Konstruktion und auffällige Details sorgen für einen Stil, der dich auf dem Platz oder unterwegs begleitet.",
+        img: "img/air-force-1-07-next-nature-se-.png"
+    },
+    {
+        id:"shoe7",
+        name: "Air Force Low Evo",
+        price: 140,
+        desc:"Bequem, langlebig und zeitlos – dieser Schuh ist aus gutem Grund die Nummer 1. Cutaway-Details zeigen ein hochwertiges Textil-Swoosh-Logo und durchgehende Air-Elemente in der Außensohle.",
+        img: "img/air-force-1-low-evo-.png"
+    },
+    {
+        id:"shoe8",
+        name: "Air Max Plus",
+        price: 140,
+        desc:"Zeig deinen Style. Das flammenähnliche Caging sorgt für Wärme auf der Straße und das luftige Mesh hält dich angenehm kühl.",
+        img: "img/air-max-plus-.png"
+    }, 
+    {
+        id:"shoe9",
+        name: "Air Max Plus 3",
+        price: 130,
+        desc:"Mit dem Nike Air Max Plus 3 machst du das Beste aus der Spielzeit. Die sichtbare Tuned-Air-Technologie unter dem Fuß verleiht dem Air Max Plus ein futuristisches Update.",
+        img: "img/air-max-plus-3-.png"
+    },
+    {
+        id:"shoe10",
+        name: "Air Max Pulse",
+        price: 190,
+        desc:"Der Air Max Pulse kombiniert urbanen Style mit robustem Design und verleiht der legendären Air Max-Linie einen Underground-Touch.",
+        img: "img/air-max-pulse-.png"
+    },
+    {
+        id:"shoe11",
+        name: "Dunk High",
+        price: 130,
+        desc:"Der gepolsterte, hoch geschnittene Schuhkragen verleiht dir einen Old-School-Look mit viel Tragekomfort.",
+        img: "img/dunk-high-.png"
+    },
+    {
+        id:"shoe12",
+        name: "Dunk Low Retro",
+        price: 114,
+        desc:"Der Nike Dunk Low Retro wurde für den Einsatz auf dem Hallenparkett entwickelt, aber fand den Weg auf die Straße.",
+        img: "img/dunk-low-retro-.png"
+    },
+];
